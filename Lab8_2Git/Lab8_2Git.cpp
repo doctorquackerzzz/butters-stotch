804f22fdc78527af67a23b284a517fd2f80cc5bf
Omenetto <nicolo.omenetto@champlain.edu>
2020-03-04 10:20:14 -0500

    Initial Commit

---
+++ Lab8_2Git/Lab8_2Git.cpp
@@ -0,0 +1,11 @@
+// Lab8_2Git.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "SUP GIT!";
+	return EXIT_SUCCESS;
+}
+
